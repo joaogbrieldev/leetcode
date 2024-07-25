756a0ca62df185eaf7b222d3021b1d8e10a466dd
jooaogbriel <joao.ggwp@outlook.com>
2024-07-25 20:54:11 -0300

    fix: entendendo new Set()

--- arrays.js
+++ arrays.js
@@ -14,7 +14,25 @@ class Solution {
     }
     return false;
   }
+
+  hasDuplicateOptmization(nums) {
+    const result = new Set();
+    for (const num of nums) {
+      if (result.has(num)) {
+        return true;
+      }
+      result.add(num);
+    }
+    return false;
+  }
 }
+
+// A versão otimizada usando Set tem complexidade O(n),
+// o que significa que o tempo de execução cresce linearmente com o tamanho do array.
+// Em contraste, a versão inicial tem complexidade O(n^2),
+// o que significa que o tempo de execução cresce quadraticamente com o tamanho do array.
+// Isso faz uma grande diferença em termos de desempenho para arrays grandes,
+// tornando a versão com Set muito mais eficiente.
 
 const nums = [1, 2, 3, 4, 5];
 const a = new Solution();
