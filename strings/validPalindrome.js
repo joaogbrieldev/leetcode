4d4bed03344d21ff068ffafbbbb935099a82a374
João Figueiredo <joao.ggwp@outlook.com>
2025-03-08 10:53:43 -0300

    feat: leetcode resolv✅

--- strings/validPalindrome.js
+++ strings/validPalindrome.js
@@ -3,16 +3,19 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-  const removeCaracters = s.replace(/[^a-zA-Z0-9 ]/g, "");
-  const removeSpaces = removeCaracters.replace(/\s/g, "");
+  const removeCaracters = s.replace(/[^a-zA-Z0-9]/g, "");
+  const removeSpaces = removeCaracters.replace(/\s/g, "").toLowerCase();
 
-  for (let i = 0; i < removeSpaces.length; i++) {
-    for (let j = removeSpaces.length - 1; j > 0; j--) {
-      if (removeSpaces[i] === removeSpaces[j]) {
-        return true;
-      }
-      return false;
+  let awser = true;
+
+  for (let i = 0, j = removeSpaces.length - 1; i < j; i++, j--) {
+    console.log("Comparando:", removeSpaces[i], "com", removeSpaces[j]);
+    if (removeSpaces[i] !== removeSpaces[j]) {
+      awser = false;
+      break;
     }
   }
+
+  return awser;
 };
 console.log(isPalindrome("A man, a plan, a canal: Panama"));
