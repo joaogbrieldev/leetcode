d7cd6577c8a81058757dcc01073b22c34a10a38b
João Figueiredo <joao.ggwp@outlook.com>
2025-03-10 19:34:25 -0300

    feat: leetcode resolv✅

--- strings/isSubsequence.js
+++ strings/isSubsequence.js
@@ -4,14 +4,19 @@
  * @return {boolean}
  */
 var isSubsequence = function (s, t) {
-  let aux = true;
-  for (let i = 0; i < s.length; i++) {
-    if (t.includes(s[i]) === false) {
-      aux = false;
-      break;
-    }
+  if (s === "" && t === "") {
+    return true;
   }
-  return aux;
+  let i = 0;
+  let j = 0;
+  while (j < t.length) {
+    if (s[i] === t[j]) {
+      i++;
+    }
+    j++;
+    if (i === s.length) return true;
+  }
+  return false;
 };
 
 console.log(isSubsequence("acb", "ahbgdc"));
