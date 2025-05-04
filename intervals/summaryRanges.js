cd5dc998215d2dc5e75419406a876efbc2f5cd9d
João Figueiredo <joao.ggwp@outlook.com>
2025-05-03 23:59:00 -0300

    feat: leetcode resolv✅

---
+++ intervals/summaryRanges.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function (nums) {
+  let result = [];
+  let i = 0;
+
+  while (i < nums.length) {
+    let start = i;
+    while (i + 1 < nums.length && nums[i] + 1 === nums[i + 1]) {
+      i++;
+    }
+    if (start === i) {
+      result.push(`${nums[i]}`);
+    } else {
+      result.push(`${nums[start]}->${nums[i]}`);
+    }
+
+    i++;
+  }
+  return result;
+};
+console.log(summaryRanges([0, 1, 2, 4, 5, 7]));
